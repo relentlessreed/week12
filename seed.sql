38bc60e9c90915eec05c8ed1d7a287cd3dcc40cd
Edward Reed <relentlessreed@gmail.com>
2021-06-06 10:18:08 -0500

    Fleshed out functionality for all CRUD operations

--- seed.sql
+++ seed.sql
@@ -19,14 +19,14 @@ VALUES ('HR Manager', 660000.00, (SELECT id FROM department WHERE name = 'HR'));
 INSERT INTO role (title, salary, department_id)
 VALUES ('HR Grunt', 660.00, (SELECT id FROM department WHERE name = 'HR'));
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id) -- This is the employee id 1
 VALUES ('John', 'Brown', (SELECT id FROM role WHERE title = 'HR Manager'), NULL);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Peter', 'Parker', (SELECT id FROM role WHERE title = 'HR Grunt'), (SELECT id FROM employee WHERE first_name = 'John' AND last_name = 'Brown'));
+VALUES ('Peter', 'Parker', (SELECT id FROM role WHERE title = 'HR Grunt'), 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('George', 'Jetson', (SELECT id FROM role WHERE title = 'HR Grunt'), (SELECT id FROM employee WHERE first_name = 'John' AND last_name = 'Brown'));
+VALUES ('George', 'Jetson', (SELECT id FROM role WHERE title = 'HR Grunt'), 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Clark', 'Kent', (SELECT id FROM role WHERE title = 'HR Grunt'), (SELECT id FROM employee WHERE first_name = 'John' AND last_name = 'Brown'));
+VALUES ('Clark', 'Kent', (SELECT id FROM role WHERE title = 'HR Grunt'), 1);
